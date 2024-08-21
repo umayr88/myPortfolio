1c9420cbdbfa5cb81d6301e766e4a633d274486f
umayr88 <islomovali6@gmail.com>
2024-08-21 13:46:18 +0200

    changes

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function updateCounter() {
 
 setInterval(updateCounter, 1000);
 updateCounter();
+
+
+document.querySelectorAll('.toggleButton').forEach(button => {
+    button.addEventListener('click', function() {
+        const projectDetails = this.nextElementSibling;
+
+        if (projectDetails.classList.contains('hidden')) {
+            projectDetails.classList.remove('hidden');
+            this.textContent = "◀ Hide " + this.textContent.split(' ')[2] + " " + this.textContent.split(' ')[3];
+            this.classList.add('open');
+        } else {
+            projectDetails.classList.add('hidden');
+            this.textContent = "▶ Show " + this.textContent.split(' ')[2] + " " + this.textContent.split(' ')[3];
+            this.classList.remove('open');
+        }
+    });
+});
